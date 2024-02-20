f40ebfa8b7d48e07f095f7985bb8b79fee7b7212
trakt <trakt@DESKTOP-UDGVRCB>
2024-02-20 22:46:59 +0200

    Добавьте файлы проекта.

---
+++ 20.02.2024/20.02.2024.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+string compressText(const string& text) {
+    string result;
+    bool wordSeen = false;
+
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] != ' ') {
+            result += text[i];
+            wordSeen = true;
+        }
+        else {
+            if (i + 1 < text.size() && text[i + 1] != ' ' && wordSeen) {
+                result += text[i];
+            }
+            else if (!wordSeen) {
+                result += text[i];
+            }
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    string text = "Hello        World   How are    you";
+    string compressedText = compressText(text); 
+    cout << compressedText << endl;
+    return 0;
+}
